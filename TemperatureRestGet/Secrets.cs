66bf65e000a5ff2dd34f89f42407468f68c990e7
Aleksander <75077931+AkRockss@users.noreply.github.com>
2022-12-14 11:53:06 +0100

    Update Secrets.cs

--- TemperatureRestGet/Secrets.cs
+++ TemperatureRestGet/Secrets.cs
@@ -3,6 +3,6 @@
     // ConnectionString to AzureDatabase: Secret wont be uploaded -This is for security purposes. 
     public class Secrets
     {
-        public static readonly string ConnectionString = @"Server=tcp:teamcool.database.windows.net,1433;Initial Catalog=TeamCool;Persist Security Info=False;User ID=teamcool;Password=Raspberry2021;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static readonly string ConnectionString = @"Server=tcp:teamcool.database.windows.net,1433;Initial Catalog=TeamCool;Persist Security Info=False;User ID=X;Password=X;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
